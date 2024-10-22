cd5f9a8a8d400c550cf6d31a68d5e165246aba17
HenriqueGalvim <64918330+HenriqueGalvim@users.noreply.github.com>
2024-10-22 11:20:17 -0400

    Missão Criar Classe Personagem -> Concluída

--- index.dart
+++ index.dart
@@ -1,44 +1,39 @@
+class Personagem {
+  String nome;
+  String raca;
+  String classe;
+  int idade;
+  double altura;
+  bool magico;
+  int vida;
+  int energia;
+  List<String> habilidades;
+
+  Personagem(this.nome, this.raca, this.classe, this.idade, this.altura,
+      this.magico, this.vida, this.energia,this.habilidades) {}
+
+  void ExibirFichaPersonagem() {
+    print("Nome: ${this.nome}");
+    print("Raça: ${this.raca}");
+    print("Classe: ${this.classe}");
+    print("Idade: ${this.idade}");
+    print("Altura: ${this.altura}");
+    if (this.magico) {
+      print("Magico: É Magico");
+    } else {
+      print("Magico: Não é mágico");
+    }
+    print("Vida: ${this.vida}");
+    print("Energia: ${this.energia}");
+    print("Lista de Habilidades");
+    for (String habilidade in this.habilidades) {
+      print(" - ${habilidade}");
+    }
+  }
+
+}
+
 void main() {
-  FichaPersonagem();
-}
-void FichaPersonagem(){
-  String nome = 'Henrique';
-  String raca = 'Humano';
-  String classe = 'Bardo';
-  int idade = 20;
-  double altura = 1.76;
-  bool indicador = false;
-  int vida = 100;
-  List<String> habilidades = ['cantar','lábia'];
-  ExibirPersonagem(nome,raca,classe,idade,altura,indicador,vida,habilidades);
-  Batalha(nome, vida);
-}
-
-void ExibirPersonagem(String nome, String raca,String classe,int idade,double altura,bool indicador,int vida,List<String> habilidades){
-  print("Nome: ${nome}");
-  print("Raca: ${raca}");
-  print("Classe: ${classe}");
-  print("Idade: ${idade}");
-  print("Altura: ${altura}");
-  if(indicador){
-    print("Indicador: Magico");
-  }else{
-    print("Indicador: Não é mágico");
-  }
-  print("Vida: ${vida}");
-  print("Lista de Habilidades");
-  for(String habilidade in habilidades){
-    print("- ${habilidade}");
-  }
-}
-
-void Batalha(String nome,int vida){
-  print("\n--- Em Batalha ---\n");
-  print("Personagem: ${nome} - Vida/Energia: ${vida}\n");
-  while (vida >= 20){
-    print("- ${nome} perdeu 20 pontos de vida/energia");
-    vida = vida - 20;
-    print("- Vida/Energia atual ${vida}\n");
-  }
-  print("--- Fim Batalha ---");
+  Personagem personagem1 = new Personagem("Henrique", "Humano", "Guerreiro", 20, 1.80, false, 300, 250, ['Golpe esmagador','Provocação','Investida Furiosa']);
+  personagem1.ExibirFichaPersonagem();
 }
