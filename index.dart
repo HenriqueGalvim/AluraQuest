20842666d43f8a49761b95b66f162845e7cb68b9
HenriqueGalvim <64918330+HenriqueGalvim@users.noreply.github.com>
2024-10-22 11:30:57 -0400

    Missão Criar Classe Guerreiro -> Concluída

--- index.dart
+++ index.dart
@@ -30,10 +30,30 @@ class Personagem {
       print(" - ${habilidade}");
     }
   }
+}
+class Guerreiro extends Personagem{
+  List<String> equipamentos;
+  String estilo;
+  Guerreiro(nome, raca, classe, idade, altura,
+      magico, vida, energia,habilidades,this.equipamentos,this.estilo): super(nome,raca,classe,idade,altura,magico,vida,energia,habilidades){}
+
+  @override
+  ExibirFichaPersonagem(){
+    print("Sou um guerreiro");
+    super.ExibirFichaPersonagem();
+    print("\nLista de Equipamentos");
+    for (String equipamento in this.equipamentos) {
+      print(" - ${equipamento}");
+    }
+    print("\nUso o estilo de combate ${this.estilo}");
+  }
 
 }
 
 void main() {
   Personagem personagem1 = new Personagem("Henrique", "Humano", "Guerreiro", 20, 1.80, false, 300, 250, ['Golpe esmagador','Provocação','Investida Furiosa']);
   personagem1.ExibirFichaPersonagem();
+  print("------ \n");
+  Guerreiro guerreiro1 = new Guerreiro("Henrique Galvim", "Anão", "Guerreiro", 20, 1.80, false, 300, 250, ['Golpe esmagador','Provocação','Investida Furiosa'],['couraça da justiça','espada do espirito'],'sentado');
+  guerreiro1.ExibirFichaPersonagem();
 }
