54850711e71c2bf26fa451e7c24746654072a4ba
HenriqueGalvim <64918330+HenriqueGalvim@users.noreply.github.com>
2024-10-22 11:41:44 -0400

    Missão Criar Classe Mago - Concluído

--- index.dart
+++ index.dart
@@ -47,7 +47,31 @@ class Guerreiro extends Personagem{
     }
     print("\nUso o estilo de combate ${this.estilo}");
   }
+}
 
+class Mago extends Personagem{
+  List<String> equipamentos;
+  List<String> feiticos;
+
+ Mago(nome, raca, classe, idade, altura,
+      magico, vida, energia,habilidades,this.equipamentos,this.feiticos): super(nome,raca,classe,idade,altura,magico,vida,energia,habilidades){}
+
+  @override
+  ExibirFichaPersonagem(){
+    print("Sou um mago");
+    super.ExibirFichaPersonagem();
+    print("\nLista de Equipamentos");
+    for (String equipamento in this.equipamentos) {
+      print(" - ${equipamento}");
+    }
+  }
+
+  LancarFeitico(){
+    print("\nLançando Feitiços");
+    for (String feitico in this.feiticos) {
+      print("- ${this.nome} usou ${feitico}");
+    }
+  }
 }
 
 void main() {
@@ -56,4 +80,8 @@ void main() {
   print("------ \n");
   Guerreiro guerreiro1 = new Guerreiro("Henrique Galvim", "Anão", "Guerreiro", 20, 1.80, false, 300, 250, ['Golpe esmagador','Provocação','Investida Furiosa'],['couraça da justiça','espada do espirito'],'sentado');
   guerreiro1.ExibirFichaPersonagem();
+  print("------ \n");
+  Mago mago1 = new Mago("Henrique dos Galvim", "Elfo", "Mago", 20, 1.80, true, 200, 350, ['Teletransporte','Previsão','Batida com cajado'],['cajado gélido','manopla de mana'],['bola de fogo','rajada de gelo']);
+  mago1.ExibirFichaPersonagem();
+  mago1.LancarFeitico();
 }
